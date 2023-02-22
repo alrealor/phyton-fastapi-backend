9d7ef8f527e1eb63c453dc732da1f6e28f7eb558
alrealor <isc.aortiz@gmail.com>
2023-02-22 14:11:35 -0600

    initial commit

---
+++ FastAPI/main.py
@@ -0,0 +1,17 @@
+from typing import Union
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+async def read_root():
+    return {"Hello": "world!"}
+
+@app.get("/items/{item_id}")
+async def read_item(item_id: int, q: Union[str, None] = None):
+    return {"item_id": item_id, "q": q}
+
+# Server start: uvicorn main:app --reload
+# Server stop: CTRL+C
+# Documentation using Swagger http://127.0.0.1:8000/docs
+# Documentation using Redocly http://127.0.0.1:8000/redoc
