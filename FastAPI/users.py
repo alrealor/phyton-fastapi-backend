55f89d8de88202b46cf45b1cd1ffb60f3a812145
alrealor <isc.aortiz@gmail.com>
2023-03-04 01:02:39 -0600

    adding operations to users controller

--- FastAPI/users.py
+++ FastAPI/users.py
@@ -1,7 +1,36 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
+# Server start: uvicorn users:app --reload
+# Server stop: CTRL+C
+# Documentation using Swagger http://127.0.0.1:8000/docs
+# Documentation using Redocly http://127.0.0.1:8000/redoc
+
+# User model
+class User(BaseModel):
+    id: int
+    name: str
+    surname: str
+    age: int
+    url: str
+
+users = [User(id=1, name="Alan", surname="Altamirano", age=36, url="https://google.com"),
+         User(id=2, name="Fernando", surname="Altamirano", age=26, url="https://google.com"),
+         User(id=3, name="Ulises", surname="Altamirano", age=35, url="https://google.com")
+         ]
+
+# get users operation
 @app.get("/users")
 async def getUsers():
-    return {"users":"user1"}
+    return users
+
+# get users by ID
+@app.get("/users/{id}")
+async def getUsersById(id: int):
+    usersById = filter(lambda user: user.id == id, users)
+    try:
+        return list(usersById)[0]
+    except:
+        return {"error":"user not found"}
--- README.txt
+++ README.txt
@@ -5,3 +5,12 @@
 # Install FastAPI and server
     $ pip install fastapi
     $ pip install "uvicorn[standard]"
+
+# Server start: 
+    $ uvicorn main:app --reload  
+    (notice that "main" is the name of the file to run)
+
+# Server stop: CTRL+C
+
+# Documentation using Swagger http://127.0.0.1:8000/docs
+# Documentation using Redocly http://127.0.0.1:8000/redoc
