0609b3e77ba5b7b59d199688ca17a8113ef7dc5b
Alan Altamirano <61994217+alrealor@users.noreply.github.com>
2023-03-08 17:47:03 -0600

    adding put and delete operations

--- FastAPI/users.py
+++ FastAPI/users.py
@@ -21,7 +21,7 @@ users = [User(id=1, name="Alan", surname="Altamirano", age=36, url="https://goog
          User(id=3, name="Ulises", surname="Altamirano", age=35, url="https://google.com")
          ]
 
-# get users operation
+# Get users operation
 @app.get("/users")
 async def getUsers():
     return users
@@ -32,21 +32,48 @@ async def getUsersById(id: int):
     return searchUser(id)
         
 # Get users by ID using query string parameter 
-@app.get("/users/")
+@app.get("/users")
 async def getUsersById(id: int):
     return searchUser(id)
 
 # Post operation to add a new user
-@app.post("/user/")
+@app.post("/users")
 async def addUser(user: User):
     if type(searchUser(user.id)) == User:
         return {"error": "user already exists"}
-    else:
-        users.append(user)
+    
+    users.append(user)
+    return user
+
+# Put operation to update a user
+@app.put("/users")
+async def updateUser(user: User):
+    found: bool = False
+    for idx, currentUser in enumerate(users):
+        if currentUser.id == user.id:
+            users[idx] = user
+            found = True
+
+    if not found:
+        return {"error":"user not found"}
+    
+    return user
+
+# Delete operation to remove a user
+@app.delete("/users/{id}")
+async def deleteUser(id: int):
+    found: bool = False
+    for idx, currentUser in enumerate(users):
+        if currentUser.id == id:
+            del users[idx]
+            found = True
+
+    if not found:
+        return {"error":"user not found"}
 
 # function to return a list of users by ID
 def searchUser(id: int):    
-    usersById = filter(lambda user: user.id == id, users)
+    usersById = filter(lambda item: item.id == id, users)
     try:
         return list(usersById)[0]
     except:
