c650192a4d536f5ac7da101a1ebb93db30655f30
Alan Altamirano <61994217+alrealor@users.noreply.github.com>
2023-03-09 17:08:09 -0600

    adding routers for multi APIs

--- FastAPI/main.py
+++ FastAPI/main.py
@@ -1,18 +1,19 @@
 from typing import Union
 from fastapi import FastAPI
-
-app = FastAPI()
+from routers import users, products
 
 # Server start: uvicorn main:app --reload
 # Server stop: CTRL+C
 # Documentation using Swagger http://127.0.0.1:8000/docs
 # Documentation using Redocly http://127.0.0.1:8000/redoc
 
+app = FastAPI()
+
+# Adding routers to the main API
+app.include_router(users.router)
+app.include_router(products.router)
+
+# Home opertion in main API
 @app.get("/")
 async def read_root():
-    return {"Hello": "world!"}
-
-@app.get("/items/{item_id}")
-async def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
-
+    return {"message": "Hello world!"}
